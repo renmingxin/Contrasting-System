61327be60d19185b297359e4a3cb7fa1fb3f73a1
renmingxin <916586942@qq.com>
2020-02-24 12:04:39 +0800

    fm

---
+++ src/view/test/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p><p>vue界面</p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    p{
+        font-size: 30px;
+    }
+</style>
